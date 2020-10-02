35a6a3c3b188022cfdde8358490ceea53804eb5c
tibin <tibin.bharatiinfotech@gmail.com>
2020-10-02 19:40:02 +0530

    todo form updated

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Input, Button } from "reactstrap";
+import { Form, Input, Button, Alert } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { v1 as uuid } from "uuid";
@@ -8,12 +8,20 @@ import { addTodo } from "../redux/action/todoActions";
 
 function TodoForm() {
   const [value, setValue] = useState("");
+  const [warning, setWarning] = useState("");
   const dispatch = useDispatch();
   const handleChange = (event) => {
     setValue(event.target.value);
   };
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (value === "") {
+      setWarning("Please type a todo name first!");
+      return;
+    }
+    else{
+      setWarning("");
+    }
     dispatch(
       addTodo({
         id: uuid(),
@@ -23,26 +31,33 @@ function TodoForm() {
     setValue("");
   };
   return (
-    <Form onSubmit={handleSubmit} className="mb-5">
-      <div className="search-panel d-flex align-items-stretch">
-        <Input
-          type="text"
-          placeholder="type todo and hit enter"
-          value={value}
-          onChange={handleChange}
-        />
-        <Button
-          type="submit"
-          color="primary"
-          className="ml-2 form_submit"
-          aria-label="add todo"
-          onClick={handleSubmit}
-        >
-          <FontAwesomeIcon icon={faPlus} className="d-sm-none d-block" />
-          <span className="d-sm-block d-none">Add Todo</span>
-        </Button>
-      </div>
-    </Form>
+    <div>
+      <Form onSubmit={handleSubmit} className="mb-5">
+        <div className="search-panel d-flex align-items-stretch">
+          <Input
+            type="text"
+            placeholder="type todo and hit enter"
+            value={value}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            className="ml-2 form_submit"
+            aria-label="add todo"
+            onClick={handleSubmit}
+          >
+            <FontAwesomeIcon icon={faPlus} className="d-sm-none d-block" />
+            <span className="d-sm-block d-none">Add Todo</span>
+          </Button>
+        </div>
+      </Form>
+      {warning && (
+        <Alert color="warning">
+          {warning}
+        </Alert>
+      )}
+    </div>
   );
 }
 
